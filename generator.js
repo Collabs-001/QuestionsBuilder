5988bc4a2c7342da36ff66c296057d06bb1cb48e
creator54 <hi.creator54@gmail.com>
2023-09-04 19:04:56 +0530

    improved generator for dynamic questions/faqs count

--- generator.js
+++ generator.js
@@ -4,95 +4,56 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const basePrompt = `
-{
-  "Topic": "[List of topics]",
-  "Difficulty": "[Specify the difficulty level]",
-  "Comment": "For each topic, provide the following:",
-  "Questions": [
-    {
-      "Number": 1,
-      "Question": "[Enter the question related to the topic]",
-      "Options": [
-        "1: [Input and expected output]",
-        "2: [Input and expected output]",
-        "3: [Input and expected output]",
-        "4: [Input and expected output]"
-      ],
-      "CorrectAnswer": "[Provide the correct answer to the question]"
-    },
-    {
-      "Number": 2,
-      "Question": "[Enter the question related to the topic]",
-      "Options": [
-        "1: [Input and expected output]",
-        "2: [Input and expected output]",
-        "3: [Input and expected output]",
-        "4: [Input and expected output]"
-      ],
-      "CorrectAnswer": "[Provide the correct answer to the question]"
-    },
-    {
-      "Number": 3,
-      "Question": "[Enter the question related to the topic]",
-      "Options": [
-        "1: [Input and expected output]",
-        "2: [Input and expected output]",
-        "3: [Input and expected output]",
-        "4: [Input and expected output]"
-      ],
-      "CorrectAnswer": "[Provide the correct answer to the question]"
-    },
-    {
-      "Number": 4,
-      "Question": "[Enter the question related to the topic]",
-      "Options": [
-        "1: [Input and expected output]",
-        "2: [Input and expected output]",
-        "3: [Input and expected output]",
-        "4: [Input and expected output]"
-      ],
-      "CorrectAnswer": "[Provide the correct answer to the question]"
-    },
-    {
-      "Number": 5,
-      "Question": "[Enter the question related to the topic]",
-      "Options": [
-        "1: [Input and expected output]",
-        "2: [Input and expected output]",
-        "3: [Input and expected output]",
-        "4: [Input and expected output]"
-      ],
-      "CorrectAnswer": "[Provide the correct answer to the question]"
-    }
-  ],
-  "FAQs": {
-    "1": {
-      "Question": "[Enter a frequently asked question related to the topic]",
-      "Answer": "[Provide the answer to the FAQ]"
-    },
-    "2": {
-      "Question": "[Enter a frequently asked question related to the topic]",
-      "Answer": "[Provide the answer to the FAQ]"
-    },
-    "3": {
-      "Question": "[Enter a frequently asked question related to the topic]",
-      "Answer": "[Provide the answer to the FAQ]"
-    },
-    "4": {
-      "Question": "[Enter a frequently asked question related to the topic]",
-      "Answer": "[Provide the answer to the FAQ]"
-    },
-    "5": {
-      "Question": "[Enter a frequently asked question related to the topic]",
-      "Answer": "[Provide the answer to the FAQ]"
-    }
-  },
-  "comment": "[Repeat the above sections for each topic, give me 5 questions and 5 FAQ's, questions can have code and in the same JSON format]"
-}`;
+function generateQuestionsJSON(questionsCount) {
+    const questions = [];
 
-export async function generateQuestions(userInput) {
+    for (let i = 1; i <= questionsCount; i++) {
+        const question = {
+            "number": i,
+            "question": "[Enter the question related to the topic]",
+            "options": [
+                "1: [Input and expected output]",
+                "2: [Input and expected output]",
+                "3: [Input and expected output]",
+                "4: [Input and expected output]"
+            ],
+            "answer": "[Provide the correct answer to the question]"
+        };
+
+        questions.push(question);
+    }
+
+    return questions;
+}
+
+function generateFAQsJSON(faqsCount) {
+    const faqs = {};
+
+    for (let i = 1; i <= faqsCount; i++) {
+        faqs[i.toString()] = {
+            "question": "[Enter a frequently asked question related to the topic]",
+            "answer": "[Provide the answer to the FAQ]"
+        };
+    }
+
+    return faqs;
+}
+
+export async function generateQuestions(userInput="frontend", questionsCount=1, faqsCount=1) {
     try {
+        const dynamicQuestions = generateQuestionsJSON(questionsCount);
+        const dynamicFAQs = generateFAQsJSON(faqsCount);
+
+        const basePrompt = `
+        {
+          "topics": "[List of topics]",
+          "difficulty": "[Specify the difficulty level]",
+          "comment": "For each topic, provide the following:",
+          "questions": ${JSON.stringify(dynamicQuestions, null, 2)},
+          "faqs": ${JSON.stringify(dynamicFAQs, null, 2)},
+          "comment": "[Repeat the above sections for each topic, give me ${questionsCount} questions and ${faqsCount} FAQ's, questions can have code and in the same JSON format, DO NOT UPDATE THE JSON KEYS, ONLY UPDATE THE VALUES]"
+        }`;
+
         const chatCompletion = await openai.chat.completions.create({
             messages: [
                 { role: "system", content: basePrompt },
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import { generateQuestions } from "./generator.js";
+import fs from 'fs';
 
-console.log(generateQuestions("fullstack,frontend").then((res)=>{ 
+console.log(generateQuestions("fullstack,frontend",5,5).then((res)=>{ 
+  fs.writeFileSync('output.json', res);
   console.log(res);
 }));
+
