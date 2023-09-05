d3ad4fa91f112347924d2e0f3e0e3848039a0e90
creator54 <hi.creator54@gmail.com>
2023-09-05 11:57:17 +0530

    UI updates + added FAQ's

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,27 @@
-import { SearchBar } from './SearchBar';
+import { SearchBar } from "./SearchBar";
 
 function Form() {
-	return (
-		<div className="flex justify-center flex-col md:flex-row gap-5">
-			<SearchBar />
-			<input
-				className="m-5 p-3 rounded"
-				type="number"
-				placeholder="FAQ's count "
-			/>
-			<input
-				className="m-5 p-3 rounded"
-				type="number"
-				placeholder="Questions count "
-			/>
-			<button
-				type="submit"
-				className="p-5 m-5 rounded bg-green-500 font-bold text-lg"
-			>
-				Generate
-			</button>
-		</div>
-	);
+  return (
+    <div className="flex justify-center flex-col md:flex-row gap-5">
+      <SearchBar />
+      <input
+        className="m-5 p-3 rounded"
+        type="number"
+        placeholder="FAQ's count "
+      />
+      <input
+        className="m-5 p-3 rounded"
+        type="number"
+        placeholder="Questions count "
+      />
+      <button
+        type="submit"
+        className="p-5 m-5 rounded bg-green-500 font-bold text-lg"
+      >
+        Generate
+      </button>
+    </div>
+  );
 }
 
 export { Form };
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,14 +1,44 @@
-import mockData from '../../output.json';
-import { RepliesList } from './index';
+import mockData from "../../output.json";
+import Faq from "react-faq-component";
+import { RepliesList } from "./index";
 
 function Reply() {
+  console.log(mockData);
+	const data = {
+		title: "Frequently Asked Questions",
+		rows: [],
+	};
+	
+	for (const key in mockData.faqs) {
+		if (mockData.faqs.hasOwnProperty(key)) {
+			const faq = mockData.faqs[key];
+			const row = {
+				title: faq.question,
+				content: faq.answer,
+			};
+			data.rows.push(row);
+		}
+	}
 
-	console.log(mockData);
-	return (
-		<section className="flex justify-center mt-10">
-			<div>
-				<RepliesList repliesList={mockData.questions} />
-				{/* <div
+  const styles = {
+    bgColor: '#a7f3d0',
+    // titleTextColor: "blue",
+    // rowTitleColor: "blue",
+    // rowContentColor: 'grey',
+    // arrowColor: "red",
+  };
+
+  const config = {
+    // animate: true,
+    // arrowIcon: "V",
+    // tabFocus: true
+  };
+
+  return (
+    <section className="flex justify-center mt-10">
+      <div>
+        <RepliesList repliesList={mockData.questions} />
+        {/* <div
 					className={`mx-5 text-justify max-w-2xl p-8 rounded-md ${repliesStyles.repliesTxt}`}
 				>
 					1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
@@ -42,9 +72,12 @@ function Reply() {
 				</div>
 				<p className="text-right mt-2 mr-5">{istDateTime}</p>
 			</div> */}
-			</div>
-		</section>
-	);
+        <div class="mx-5 text-justify max-w-2xl p-8 rounded-md _repliesTxt_pbitv_1">
+          <Faq data={data} styles={styles} config={config} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export { Reply };
