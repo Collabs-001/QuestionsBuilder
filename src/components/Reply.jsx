7335ab2f5f12a997f0d7a1c0b8c026118ade49fa
creator54 <hi.creator54@gmail.com>
2023-09-05 22:33:51 +0530

    fixup's

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -3,45 +3,50 @@ import Faq from 'react-faq-component';
 import { RepliesList } from './index';
 
 function Reply({ repliesList }) {
-	const faqs = repliesList?.faqs || {};
-	const data = {
-		title: 'Frequently Asked Questions',
-		rows: Object.keys(faqs).map((key) => ({
-			title: faqs[key]?.question,
-			content: faqs[key]?.answer,
-		})),
-	};
+  const faqs = repliesList?.faqs || {};
+  const data = {
+    title: 'Frequently Asked Questions',
+    rows: Object.keys(faqs).map((key) => ({
+      title: faqs[key]?.question,
+      content: faqs[key]?.answer,
+    })),
+  };
 
-	const styles = {
-		bgColor: 'white',
-		titleTextColor: 'black',
-		rowTitleColor: 'blue',
-		rowContentColor: 'grey',
-		arrowColor: 'blue',
-	};
+  const styles = {
+    bgColor: 'white', // Background color
+    titleTextColor: 'black', // Title text color
+    rowTitleColor: 'blue', // Row title text color
+    rowContentColor: 'grey', // Row content text color
+    arrowColor: 'blue', // Arrow color
+  };
 
 	const config = {
-		animate: true,
-		arrowIcon: 'V',
-		tabFocus: true,
-	};
+    animate: true,
+    arrowIcon: "V",
+    openOnload: 0,
+    expandIcon: "+",
+    collapseIcon: "-",
+};
 
-	return (
-		<section className="mt-10">
-			<div className="container mx-auto px-4">
-				<div className="flex flex-wrap -mx-4">
-					<div className="w-full lg:w-1/2 px-4 mb-6">
-						<RepliesList repliesList={repliesList?.questions} />
-					</div>
-					<div className="w-full lg:w-1/2 px-4">
-						<div className="max-w-2xl mx-auto">
-							<Faq data={data} styles={styles} config={config} />
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="mt-10">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full lg:w-1/2 px-4 mb-6">
+						<p className='text-xl font-bold mt-3 mb-5'>
+							Difficulty: {repliesList?.difficulty}
+						</p>
+            <RepliesList repliesList={repliesList?.questions} />
+          </div>
+          <div className="w-full lg:w-1/2 px-4">
+            <div className="max-w-2xl mx-auto">
+              <Faq data={data} styles={styles} config={config} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export { Reply };
