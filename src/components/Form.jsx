822275dd4251f4d83fc347e63be64d17f6e342d8
creator54 <hi.creator54@gmail.com>
2023-09-05 21:20:45 +0530

    Add Button to save JSON

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ function Form({
   setSearchTopic,
   setQuestionsCount,
 }) {
-  const [chatGPTRes, setChatGPTRes] = useState(null); // Set initial state to null
+  const [chatGPTRes, setChatGPTRes] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -40,6 +40,19 @@ function Form({
 
   function setSearchTopicHandler(event) {
     setSearchTopic(event.target.value);
+  }
+
+  // Function to save JSON data to a file
+  function saveJSONToFile(jsonData) {
+    const blob = new Blob([JSON.stringify(jsonData, null, 2)], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "output.json";
+    a.click();
+    URL.revokeObjectURL(url);
   }
 
   return (
@@ -79,27 +92,43 @@ function Form({
         </button>
       </div>
 
-      {/* Only display "No result" when chatGPTRes is empty and there is no error */}
-      {chatGPTRes !== null &&
-        !loading &&
-        Object.keys(chatGPTRes).length === 0 &&
-        !error && (
-          <div className="text-center mt-5 font-bold text-2xl">
-						Could not generate questions for the given topic. Please try again !
-					</div>
-        )}
+      {chatGPTRes !== null && (
+        <>
+          {loading && (
+            <div className="text-center mt-5 font-bold text-2xl">
+              Loading...
+            </div>
+          )}
+          {!loading && Object.keys(chatGPTRes).length === 0 && !error && (
+            <div className="text-center mt-5 font-bold text-2xl">
+              Could not generate questions for the given topic. Please try
+              again!
+            </div>
+          )}
 
-      {/* Remove the "No result" message when there is an error */}
-      {!loading && error && (
-        <div className="text-center mt-5 text-red-500 font-bold text-2xl">
-          Error: Unable to fetch data
-        </div>
+          {!loading && error && (
+            <div className="text-center mt-5 text-red-500 font-bold text-2xl">
+              Error: Unable to fetch data
+            </div>
+          )}
+
+          {!loading && Object.keys(chatGPTRes).length > 0 && (
+            <>
+              <Reply repliesList={chatGPTRes} />
+              <div className="text-center mt-3">
+                <button
+                  type="button"
+                  onClick={() => saveJSONToFile(chatGPTRes)}
+                  className="flex-1 p-3 rounded-lg bg-green-500 text-white font-bold text-lg mt-3"
+                  disabled={!chatGPTRes}
+                >
+                  Save JSON
+                </button>
+              </div>
+            </>
+          )}
+        </>
       )}
-      {!loading &&
-        chatGPTRes !== null &&
-        Object.keys(chatGPTRes).length > 0 && (
-          <Reply repliesList={chatGPTRes} />
-        )}
     </div>
   );
 }
