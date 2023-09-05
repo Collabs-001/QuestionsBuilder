cdc95d4effdbb9f1d78ca7c5de9735040509c28d
creator54 <hi.creator54@gmail.com>
2023-09-05 16:49:47 +0530

    disable Generate if request pending

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,73 +4,73 @@ import { SearchBar } from './SearchBar';
 import { generateQuestions } from '../../generator';
 
 function Form({
-	noOfFaqs,
-	noOfQuestions,
-	searchTopic,
-	setFaqsCount,
-	setSearchTopic,
-	setQuestionsCount,
+  noOfFaqs,
+  noOfQuestions,
+  searchTopic,
+  setFaqsCount,
+  setSearchTopic,
+  setQuestionsCount,
 }) {
-	const [chatGPTRes, setChatGPTRes] = useState({});
+  const [chatGPTRes, setChatGPTRes] = useState({});
+  const [loading, setLoading] = useState(false); // Add a loading state
 
-	function submitFormHandler(event) {
-		event.preventDefault();
-		// fetch('../../output.json')
-		generateQuestions(searchTopic, noOfQuestions, noOfFaqs)
-			.then((res) => {
-				return res;
-			})
-			.then((data) => {
-				console.log(data);
-				setChatGPTRes(data);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+  function submitFormHandler(event) {
+    event.preventDefault();
+    setLoading(true); // Start loading
 
-	useEffect(() => {
-		console.log('CHANGED');
-	}, [chatGPTRes]);
+    generateQuestions(searchTopic, noOfQuestions, noOfFaqs)
+      .then((data) => {
+        console.log(data);
+        setChatGPTRes(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false); // Stop loading, whether the request succeeds or fails
+      });
+  }
 
-	function setSearchTopicHandler(event) {
-		setSearchTopic(event.target.value);
-	}
+  useEffect(() => {
+    console.log('CHANGED');
+  }, [chatGPTRes]);
 
-	return (
-		<>
-			<div className="flex justify-center flex-col md:flex-row gap-5">
-				<SearchBar
-					searchTopic={searchTopic}
-					setSearchTopicHandler={setSearchTopicHandler}
-				/>
-				<input
-					className="m-5 p-3 rounded"
-					type="number"
-					placeholder="FAQ's count "
-					value={noOfFaqs}
-					onChange={(e) => setFaqsCount(e.target.value)}
-				/>
-			</div>
-			<div className="flex justify-center flex-col md:flex-row gap-5">
-				<input
-					className="m-5 p-3 rounded"
-					type="number"
-					placeholder="Questions count "
-					value={noOfQuestions}
-					onChange={(e) => setQuestionsCount(e.target.value)}
-				/>
-				<button
-					type="submit"
-					onClick={(e) => submitFormHandler(e)}
-					className="p-5 m-5 rounded bg-green-500 font-bold text-lg"
-				>
-					Generate
-				</button>
-			</div>
-			{Object.keys(chatGPTRes).length > 0 && <Reply repliesList={chatGPTRes} />}
-		</>
-	);
+  function setSearchTopicHandler(event) {
+    setSearchTopic(event.target.value);
+  }
+
+  return (
+    <>
+      <div className="flex justify-center flex-col md:flex-row gap-5">
+        <SearchBar searchTopic={searchTopic} setSearchTopicHandler={setSearchTopicHandler} />
+        <input
+          className="m-5 p-3 rounded"
+          type="number"
+          placeholder="FAQ's count"
+          value={noOfFaqs}
+          onChange={(e) => setFaqsCount(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-center flex-col md:flex-row gap-5">
+        <input
+          className="m-5 p-3 rounded"
+          type="number"
+          placeholder="Questions count"
+          value={noOfQuestions}
+          onChange={(e) => setQuestionsCount(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={(e) => submitFormHandler(e)}
+          className={`p-5 m-5 rounded bg-green-500 font-bold text-lg ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+          disabled={loading} // Disable the button when loading is true
+        >
+          {loading ? 'Generating...' : 'Generate'}
+        </button>
+      </div>
+      {Object.keys(chatGPTRes).length > 0 && <Reply repliesList={chatGPTRes} />}
+    </>
+  );
 }
 
 export { Form };
